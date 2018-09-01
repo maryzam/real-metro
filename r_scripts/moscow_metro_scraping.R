334ab7d73d1d9859a558be2bda9c90f1e809566b
maryzam <zamo.code@gmail.com>
2018-09-01 16:47:35 +0300

    Add data set of moscow metro stations

--- r_scripts/moscow_metro_scraping.R
+++ r_scripts/moscow_metro_scraping.R
@@ -1,5 +1,6 @@
 
 library('rvest')
+library('readr')
 
 Sys.setlocale("LC_CTYPE", "russian")
 
@@ -10,7 +11,19 @@ stations <- page %>%
   html_node("table.sortable") %>%
   html_table(header = FALSE)
 
-names(stations) <- c("Line", "Name", "OriginName", "Transfer", "Opened", "Elev.", "Type", "Coords", "Pic")
+names(stations) <- c("Line", "Name", "OriginName", "Transfer", "Opened", "Elev", "Type", "Coords", "Pic")
 
 dropped <- c("Type", "Pic", "Transfer")
+stations <- stations[-c(1), !(colnames(stations) %in% dropped) ]
+
+stations$Line <- gsub("\u2009", "", stations$Line)
+stations$Elev <- parse_number(gsub("\u2212", "-", stations$Elev))
+
+#parse coords
+stations <- stations %>%
+  mutate(Coords2 = unlist(strsplit(Coords, "/"))[3]) %>%
+  mutate(Lat = parse_number(unlist(strsplit(Coords2, "; "))[1])) %>%
+  mutate(Lon = parse_number(unlist(strsplit(Coords2, "; "))[2]))
+
+dropped <- c("Coords", "Coords2")
 stations <- stations[-c(1), !(colnames(stations) %in% dropped) ]
